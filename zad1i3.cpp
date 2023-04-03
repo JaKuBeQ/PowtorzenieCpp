19a5ca4c45a6f2dbd9af0fcbb766150e9ab5fd3d
JakubeQ <63652706+JaKuBeQ@users.noreply.github.com>
2023-04-03 19:45:49 +0200

    Add files via upload

--- zad1i3.cpp
+++ zad1i3.cpp
@@ -1,29 +1,32 @@
 #include <iostream>
-#include <climits>
+#include <climits> // Biblioteka umożliwiająca korzystanie z stałej najwiekszej i najmniejszej INT_MIN.
 
 using namespace std;
 
 int main() {
     int x;
-    cout << "Podaj ilosc liczb do wczytania: ";
-    cin >> x;
+    cout << "Podaj ilosc liczb do wczytania: "; // Wyświetlenie tekstu z prośbą o podanie ilości liczb.
+    cin >> x; // Pobranie od użytkownika ilości liczb.
 
-    int najwieksza = INT_MIN;
+    int najwieksza = INT_MIN; // zmienna najwieksza  = najmniejszej mozliwej liczbie
     int ilosc = 0;
-    for (int i = 0; i < x; i++) {
+
+    for (int i = 0; i < x; i++) { // petla wykonujaca sie x razy
         int liczba;
-        cout << "Podaj liczbe: ";
-        cin >> liczba;
+        cout << "Podaj liczbe: "; // pytanie o podanie liczby
+        cin >> liczba; // Pobranie od użytkownika liczby.
+
         if (liczba > najwieksza) {
-            najwieksza = liczba;
-            ilosc=1;
-        }else if(liczba==najwieksza){
-            ilosc++;
+            najwieksza = liczba; // przypisanie liczba do najwiekszej
+            ilosc=1; // jesli liczba jest wieksza niz najwieksza wystapienie = 1
+        } else if(liczba==najwieksza){ 
+            ilosc++; // jesli liczba jest rowna najwieksza wystapienie wiecej niz 1
         }
     }
 
+    // Wyświetlenie największej liczby i jej ilości wystąpień.
     cout << "Najwieksza liczba to: " << najwieksza << endl;
     cout << "Wystapila " << ilosc << " razy" << endl;
 
-    return 0;
+    return 0; 
 }
--- zad2i4.cpp
+++ zad2i4.cpp
@@ -1,27 +1,30 @@
 #include <iostream>
-#include <climits>
+#include <climits> // Biblioteka umożliwiająca korzystanie z stałej najwiekszej i najmniejszej INT_MIN.
 
 using namespace std;
 
 int main() {
     int x;
-    cout << "Podaj ilosc liczb do wczytania: ";
-    cin >> x;
+    cout << "Podaj ilosc liczb do wczytania: "; // Wyświetlenie tekstu z prośbą o podanie ilości liczb.
+    cin >> x; // Pobranie od użytkownika ilości liczb.
 
-    int najmniejsza = INT_MAX;
+    int najmniejsza = INT_MAX; //zmienna najmniejsza  = najwiekszej mozliwej liczbie
     int ilosc = 0;
-    for (int i = 0; i < x; i++) {
+
+    for (int i = 0; i < x; i++) { // petla wykonujaca sie x razy
         int liczba;
-        cout << "Podaj liczbe: ";
-        cin >> liczba;
+        cout << "Podaj liczbe: "; // pytanie o podanie liczby
+        cin >> liczba; // Pobranie od użytkownika liczby.
+
         if (liczba < najmniejsza) {
-            najmniejsza = liczba;
-            ilosc=1;
-        }else if(liczba==najmniejsza){
-            ilosc++;
+            najmniejsza = liczba; //przypisanie liczba do najmniejsza
+            ilosc=1; // jesli liczba jest wieksza niz najwieksza wystapienie = 1
+        } else if(liczba==najmniejsza){
+            ilosc++; // jesli liczba jest rowna najwieksza wystapienie wiecej niz 1
         }
     }
 
+    // Wyświetlenie najmniejsxej liczby i jej ilości wystąpień.
     cout << "Najmniejsza liczba to: " << najmniejsza << endl;
     cout << "Wystapila " << ilosc << " razy" << endl;
 
