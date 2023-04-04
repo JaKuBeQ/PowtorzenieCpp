21a80fd9a7d6d7a1e5574f6a6763c6f78f401682
JakubeQ <63652706+JaKuBeQ@users.noreply.github.com>
2023-04-04 12:12:55 +0200

    Update zad2i4.cpp

--- zad2i4.cpp
+++ zad2i4.cpp
@@ -4,28 +4,28 @@
 using namespace std;
 
 int main() {
-    int x;
+    int liczba;
     cout << "Podaj ilosc liczb do wczytania: "; // Wyświetlenie tekstu z prośbą o podanie ilości liczb.
-    cin >> x; // Pobranie od użytkownika ilości liczb.
+    cin >> liczba; // Pobranie od użytkownika ilości liczb.
 
     int najmniejsza = INT_MAX; //zmienna najmniejsza  = najwiekszej mozliwej liczbie
     int ilosc = 0;
 
-    for (int i = 0; i < x; i++) { // petla wykonujaca sie x razy
-        int liczba;
+    for (int i = 0; i < liczba; i++) { // petla wykonujaca sie x razy
+        int liczba2;
         cout << "Podaj liczbe: "; // pytanie o podanie liczby
-        cin >> liczba; // Pobranie od użytkownika liczby.
+        cin >> liczba2; // Pobranie od użytkownika liczby.
 
-        if (liczba < najmniejsza) {
-            najmniejsza = liczba; //przypisanie liczba do najmniejsza
+        if (liczba2 < najmniejsza) {
+            najmniejsza = liczba2; //przypisanie liczba do najmniejsza
             ilosc=1; // jesli liczba jest wieksza niz najwieksza wystapienie = 1
-        } else if(liczba==najmniejsza){
+        } else if(liczba2==najmniejsza){
             ilosc++; // jesli liczba jest rowna najwieksza wystapienie wiecej niz 1
         }
     }
 
     // Wyświetlenie najmniejsxej liczby i jej ilości wystąpień.
-    cout << "Najmniejsza liczba to: " << najmniejsza << endl;
+    cout << "Najmniejsza liczba to: " << najmniejsza << " Wystapila " << ilosc << " razy" << endl;
     cout << "Wystapila " << ilosc << " razy" << endl;
 
     return 0;
